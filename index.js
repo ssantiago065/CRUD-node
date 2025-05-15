c2ad1fbac97c67cb1138730f47a69c8f056bd94d
Sergio <ssantiago065@gmail.com>
2025-05-14 23:09:09 -0600

    Add post method to test request and add express.json middelware to send and receive json

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+app.use(express.json())
+
 
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 3000;
@@ -20,3 +22,8 @@ mongoose.connect(uri)
 app.get('/', (req, res) => {
     res.send('Hello from node API')
 }) 
+
+app.post("/api/product", (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
+})
