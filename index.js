26e8639c1543a8cff2f1265d0876de8def7e5a36
Sergio <ssantiago065@gmail.com>
2025-05-16 19:30:30 -0600

    Add put method to update product by id

--- index.js
+++ index.js
@@ -56,3 +56,20 @@ app.post("/api/products", async (req, res) => {
         res.status(500).json({message: error.message})
     }
 })
+
+app.put("/api/products/:id", async (req, res) => {
+    try{
+        const { id } = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+
+        if(!product){
+            return res.status(400).json({message: "Product not found"})
+        }
+
+        const updatedProduct = await Product.findById(id)
+        res.status(200).json(updatedProduct)
+
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})  
