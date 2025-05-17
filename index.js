1e27be1e55cbe511fed4303c4de3ac17667c0a6b
Sergio <ssantiago065@gmail.com>
2025-05-16 19:09:44 -0600

    Update post products to save new product on database and Add get method on same products route to see all products on database

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/product.model.js')
 const app = express()
+
+
+
 app.use(express.json())
 
 
@@ -23,7 +27,21 @@ app.get('/', (req, res) => {
     res.send('Hello from node API')
 }) 
 
-app.post("/api/product", (req, res) => {
-    console.log(req.body)
-    res.send(req.body)
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post("/api/products", async (req, res) => {
+    try{
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
 })
--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,4 @@
-const mongoose = requiere("mongoose")
+const mongoose = require("mongoose")
 
 const ProductSchema = mongoose.Schema(
     {
