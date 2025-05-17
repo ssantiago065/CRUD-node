0c610a3251e204329de82543f20c7079b1920bcd
Sergio <ssantiago065@gmail.com>
2025-05-16 19:20:51 -0600

    Add get method to get a single product by id using products route

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(uri)
 .then(() => {
     console.log("Connected to DB")
     app.listen(port, () => {
-        console.log('Server is running on port 3000');
+        console.log("Server is running on port 3000");
     });
 })
 .catch(() => {
@@ -24,16 +24,27 @@ mongoose.connect(uri)
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello from node API')
+    res.send("Hello from node API")
 }) 
 
-app.get('/api/products', async (req, res) => {
+app.get("/api/products" , async (req, res) => {
     try {
         const products = await Product.find({})
         res.status(200).json(products)
 
     }catch (error){
         res.status(500).json({message: error.message})
+    }
+})
+
+app.get("/api/products/:id", async (req, res) => {
+    try{
+        const { id } = req.params
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+
+    }catch (error){
+        response.status(500).json({message: error.message})
     }
 })
 
